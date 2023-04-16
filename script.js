adeccc6003a67992d083c8a1fddbd95d8b67396d
Vaidy05 <92988611+Vaidy05@users.noreply.github.com>
2023-04-16 14:25:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function volume_sphere() {
     //Write your code here
-  
+  let item = document.getElementById("radius").value;
+  let vol = Math.pow(Number(item),3)* 4/3 * 3.14;
+  let volume = document.getElementById("volume");
+  volume.value=vol;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
